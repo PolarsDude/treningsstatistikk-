e66083ea455351bf989486a4b62347167e179578
Yafee Ishraq <yafee5@gmail.com>
2025-05-08 10:55:46 +0200

    Laget funksjon for å lage git antall kombinasjoner

--- pages/Kombinasjon statistikk.py
+++ pages/Kombinasjon statistikk.py
@@ -51,37 +51,57 @@ df = (df
 )
 
 
-# beregner alle trippletter
-triplett = (df
-.filter(pl.col('Avlyst trening')=='Nei',
-        pl.col('Deltok')=='Ja')
-.select('Dato','Navn')
-.unique()
-.group_by('Dato')
-.agg(pl.col('Navn').alias('navn_1'),
-     pl.col('Navn').alias('navn_2'),
-     pl.col('Navn').alias('navn_3')
-)
+# Funksjon for å få statistikk på kombinasjoner. Dersom antall_kombinasjon er lik 3 betyr det at vi er interessert i tripplett
+def kombi_statistikk(antall_kombinasjon):
 
-# Finner 3 kombinasjoner
-.explode('navn_1')
-.explode('navn_2')
-.explode('navn_3')
+ # Lager antall navn expr
+ navn_init =[f'navn_{i+1}' for i in range(antall_kombinasjon)]
+ navn_expr = [pl.col('Navn').alias(f'navn_{i+1}') for i in range(antall_kombinasjon)]
+ 
+ # Initialiserer datasett
+ df_init = (df
+ .filter(pl.col('Avlyst trening')=='Nei',
+         pl.col('Deltok')=='Ja')
+ .select('Dato','Navn')
+ .unique()
+ .group_by('Dato')
+ .agg(*navn_expr)
+ )
+ 
+ # Exploder df for å finne alle kombinasjoner 
+ for i in range(antall_kombinasjon):
+    df_init = df_init.explode(navn_init[i])
+ 
+ # Fjerner duplikater 
+ df_kombi = (
+ 
+ df_init
+ 
+ # Skal samle alle navnene i en liste for å telle unike navn. Skal fjerne lister som har kun ett navn        
+ .with_columns(pl.concat_str(*navn_init, separator=',').str.split(",").alias('kombi'))
+ 
+ # Sorterer liste
+ .with_columns(pl.col('kombi').list.sort())
+ 
+ # Filtrerer på lister som har x antall unike navn (etter at duplikater er fjernet)
+ .filter(pl.col('kombi').list.unique().list.n_unique()==antall_kombinasjon)
+ .group_by('kombi')
+ .agg(pl.col('Dato').n_unique().alias('Antall treninger'))
+ .sort(by = ['Antall treninger'],descending=True)
+ .with_columns(pl.col('kombi').list.join(",").alias('Kombinasjon'))
+ .select('Kombinasjon','Antall treninger')
+ 
+ )
 
-# Skal samle alle navnene i en liste for å telle unike navn. Skal fjerne lister som har kun ett navn        
-.with_columns(pl.concat_str(['navn_1', 'navn_2', 'navn_3'], separator=',').str.split(",").alias('kombi'))
+ return df_kombi
 
-# Sorterer liste
-.with_columns(pl.col('kombi').list.sort())
 
-# Filtrerer på lister som har 3 unike navn (etter at duplikater er fjernet)
-.filter(pl.col('kombi').list.unique().list.n_unique()==3)
-.group_by('kombi')
-.agg(pl.col('Dato').n_unique().alias('Antall treninger'))
-.sort(by = ['Antall treninger'],descending=True)
-.with_columns(pl.col('kombi').list.join(",").alias('Kombinasjon'))
-.select('Kombinasjon','Antall treninger')
-)
 
-st.write("Trippletter som har vært på trening")
-st.dataframe(triplett)
+# Lager valg på antall kombinasjoner
+velg_antall_kombi = st.selectbox("Velg antall kombinasjoner (maks 6 kombinasjoner tillatt):", range(1,7),key="velg_antall_kombi")
+
+# Kjør funksjon
+df_kombi = kombi_statistikk(st.session_state.velg_antall_kombi)
+
+st.write("Alle kombinasjoner med antall treninger:")
+st.dataframe(df_kombi)
