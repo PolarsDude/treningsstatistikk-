34111d7c4f12c5cabf98381d1711358d4615db1d
Yafee Ishraq <yafee5@gmail.com>
2025-05-09 10:05:55 +0200

    Laget en prep funksjon

--- pages/Kombinasjon statistikk.py
+++ pages/Kombinasjon statistikk.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import polars as pl
+from data_prep import prep
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -15,40 +16,8 @@ st.markdown(
 df = pl.read_excel("treningsdata.xlsx")
 
 
-# Fikser på dato kolonne
-df = (df
-.with_columns(pl.col('Dato').cast(pl.String).str.slice(0,4).alias('År'),
-              pl.col('Dato').cast(pl.String).str.slice(4,2).alias('Måned'),
-              pl.col('Dato').cast(pl.String).str.slice(6,2).alias('Dag')
-              )
-
-# Gjør dag om k
-.with_columns(pl.concat_str(['År', 'Måned', 'Dag'], separator='-').cast(pl.Date).alias('Dato'),
-              pl.col('Måned','Dag').cast(pl.Int64)
-              )
-)
-
-
-# Mapper måned nr til navn
-df = (df
-    .with_columns(pl.col("Måned").alias('Måned nr'))
-    .with_columns(
-     pl.when(pl.col("Måned") == 1).then(pl.lit("Januar"))
-     .when(pl.col("Måned") == 2).then(pl.lit("Februar"))
-     .when(pl.col("Måned") == 3).then(pl.lit("Mars"))
-     .when(pl.col("Måned") == 4).then(pl.lit("April"))
-     .when(pl.col("Måned") == 5).then(pl.lit("Mai"))
-     .when(pl.col("Måned") == 6).then(pl.lit("Juni"))
-     .when(pl.col("Måned") == 7).then(pl.lit("Juli"))
-     .when(pl.col("Måned") == 8).then(pl.lit("August"))
-     .when(pl.col("Måned") == 9).then(pl.lit("September"))
-     .when(pl.col("Måned") == 10).then(pl.lit("Oktober"))
-     .when(pl.col("Måned") == 11).then(pl.lit("November"))
-     .when(pl.col("Måned") == 12).then(pl.lit("Desember"))
-     .otherwise(pl.lit("Ukjent"))
-     .alias("Måned")
-    )
-)
+# Transformer dataene
+df = prep(df)
 
 
 # Funksjon for å få statistikk på kombinasjoner. Dersom antall_kombinasjon er lik 3 betyr det at vi er interessert i tripplett
