87016c0430155639cec55f4403225b0d46890ef8
Yafee Ishraq <yafee5@gmail.com>
2025-05-10 16:06:59 +0200

    Lagt inn visualisering på antall treninger og antall avlyste treninger

--- pages/Generell statistikk.py
+++ pages/Generell statistikk.py
@@ -47,8 +47,29 @@ antall_treninger_per_mnd = (df
 st.write("Antall treninger per måned")
 st.dataframe(antall_treninger_per_mnd)
 
+antall_treninger = (df
+.group_by('Måned','Måned nr')
+.agg(pl.col('Dato').filter(pl.col('Avlyst trening')=='Nei').n_unique().alias('Antall treninger'),
+     pl.col('Dato').filter(pl.col('Avlyst trening')=='Ja').n_unique().alias('Antall avlyste treninger'))
+)
 
-#df = pl.read_excel("../treningsdata.xlsx")
-#df.head()
+antall_treninger = (antall_treninger
+.unpivot(index = ['Måned','Måned nr'],on = ['Antall treninger','Antall avlyste treninger'])
+.sort(by=['variable','Måned nr'])
+.drop('Måned nr')
+)
 
-#df.group_by('År','Måned','Måned nr').agg(pl.col())
+
+fig, ax = plt.subplots()
+sns.lineplot(data = antall_treninger.to_pandas(), x='Måned', y='value',hue='variable',ax = ax)
+
+# Roter x-labels
+ax.tick_params(axis='x', rotation=50)
+ax.set_ylabel("")
+ax.set_xlabel("")
+ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+ax.set_title('Antall treninger per måned')
+
+
+# Viser figuren i Streamlit
+st.pyplot(fig)
