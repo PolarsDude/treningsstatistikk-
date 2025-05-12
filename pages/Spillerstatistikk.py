a686870779a81c1989e356656319931a86412397
Yafee Ishraq <yafee5@gmail.com>
2025-05-12 12:09:17 +0200

    Lagt inn streak statistikk

--- pages/Spillerstatistikk.py
+++ pages/Spillerstatistikk.py
@@ -1,4 +1,5 @@
 import polars as pl
+import polars_ds as pds
 from utils import prep
 import streamlit as st
 import seaborn as sns
@@ -39,3 +40,32 @@ ax.tick_params(axis='x', rotation=50)
 
 # Viser figuren i Streamlit
 st.pyplot(fig)
+
+######## Lager statistikk for treningsstreak ##
+
+
+# Beskrivelse av appen
+st.markdown(
+    """
+    Denne tabellen viser ulike nøkkeltall for en valgt spiller
+    * Totalt antall treninger viser hvor mange treninger spilleren har deltatt på totallt
+    * Første trening og siste trening viser henholdsvis første og siste trening spilleren har deltatt på
+    * Treningsstreak viser hvor mange treninger på rad en deltaker har møtt opp på uten å ha noen fravær mellom. 
+    Hver gang en deltaker ikke møter, nullstilles streaken, og den starter på nytt neste gang vedkommende møter opp.
+    """
+)
+
+treningsstreak = (df
+.filter(pl.col('Avlyst trening')=='Nei',
+        pl.col('Navn')==st.session_state.velg_person
+        )       
+.group_by('Navn')
+.agg(pl.col('Dato').filter(pl.col('Deltok')=='Ja').n_unique().alias('Totalt antall treninger'),
+     pl.col('Dato').filter(pl.col('Deltok')=='Ja').min().cast(pl.Date).alias('Første trening'),
+     pl.col('Dato').filter(pl.col('Deltok')=='Ja').max().cast(pl.Date).alias('Siste trening'),
+     pds.query_longest_streak(pl.col('Deltok')=='Ja').alias('Streak')
+     )
+)
+
+st.write("Antall treninger per måned")
+st.dataframe(treningsstreak)
