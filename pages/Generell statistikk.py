179ac73067dea1782d24bd55f1aee4f2a15dece6
Yafee Ishraq <yafee5@gmail.com>
2025-05-20 15:24:50 +0200

    Plottet 2024 og 2025, men det er feil i grafen. Må koble på første mandagen etter publiseringsdato

--- pages/Generell statistikk.py
+++ pages/Generell statistikk.py
@@ -48,20 +48,45 @@ st.write("Antall treninger per måned")
 st.dataframe(antall_treninger_per_mnd)
 
 antall_treninger = (df
-.group_by('Måned','Måned nr')
+.group_by('År','Måned','Måned nr')
 .agg(pl.col('Dato').filter(pl.col('Avlyst trening')=='Nei').n_unique().alias('Antall treninger'),
      pl.col('Dato').filter(pl.col('Avlyst trening')=='Ja').n_unique().alias('Antall avlyste treninger'))
 )
 
 antall_treninger = (antall_treninger
-.unpivot(index = ['Måned','Måned nr'],on = ['Antall treninger','Antall avlyste treninger'])
-.sort(by=['variable','Måned nr'])
-.drop('Måned nr')
+.unpivot(index = ['År','Måned','Måned nr'],on = ['Antall treninger','Antall avlyste treninger'])
+.sort(by=['variable','År','Måned nr'])
+)
+
+# Skal plotte fra den første dagen per måned
+antall_treninger = (
+    antall_treninger
+    .with_columns(
+        pl.when(pl.col('Måned nr').cast(pl.String).str.len_chars() > 1)
+        .then(
+            pl.concat_str(
+                [pl.col('År'), pl.col('Måned nr'), pl.lit('01')],
+                separator="-"
+            )
+        )
+        .otherwise(
+            pl.concat_str(
+                [
+                    pl.col('År'),
+                    pl.concat_str([pl.lit('0'), pl.col('Måned nr')]),
+                    pl.lit('01')
+                ],
+                separator="-"
+            )
+        )
+        .cast(pl.Date)
+        .alias('d')  # valgfritt: gir navnet 'd' til kolonnen
+    )
 )
 
 
 fig, ax = plt.subplots()
-sns.lineplot(data = antall_treninger.to_pandas(), x='Måned', y='value',hue='variable',ax = ax)
+sns.lineplot(data = antall_treninger.to_pandas(), x='d', y='value',hue='variable',ax = ax)
 
 # Roter x-labels
 ax.tick_params(axis='x', rotation=50)
